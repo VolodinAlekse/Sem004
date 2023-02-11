517876ea10314814fdc76166bb6e18effa24d688
VolodinAlekse <volodin.8989@mail.ru>
2023-02-11 18:04:55 +0300

    demo_026_028

---
+++ demo_026_028/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 26: Напишите программу, 
+// которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// String GetNumber(string text);
+
+// {
+//     System.Console.WriteLine();
+//     return Console.ReadLine();
+// }
+
+// string a = GetNumber("Введите число n");
+// int len = a.Length;
+// System.Console.WriteLine(len);
+
+
+
+// Задача 28: Напишите программу,
+//  которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int GetNumber(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Multiply(int x)
+{
+    int result = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+
+int num = GetNumber("Введите число: ");
+System.Console.WriteLine($"Произведение чисел от 1 до {num} = {Multiply(num)}");
