49364c209daf547cf090f3be6e96ac3045d6860c
VolodinAlekse <volodin.8989@mail.ru>
2023-02-12 01:24:57 +0300

    home_029

--- demo_030/Program.cs
+++ demo_030/Program.cs
@@ -27,31 +27,30 @@
 
 
 
-int GetNumber(string text)
-{
-    System.Console.Write(text);
-    return Convert.ToInt32(Console.ReadLine());
-}
+// int GetNumber(string text)
+// {
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
 
-int[] GetArray()
-{
-    int[] arr = new int[8];
-    var rand = new Random();
-    for (int i = 0; i < arr.Length; i++)
-    {
-        arr[i] = rand.Next(0, 2);
-    }
-    return arr;
-}
+// int[] GetArray()
+// {
+//     int[] arr = new int[8];
+//     var rand = new Random();
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = rand.Next(0, 2);
+//     }
+//     return arr;
+// }
 
-void PrintArray(int[] arr)
-{
-    // for (int i = 0; i < arr.Length; i++)
-    // {
-    //     System.Console.Write(arr[i] + " ");
-    // }
-    System.Console.WriteLine("["+string.Join(", ", arr)+"]");
-}
+// void PrintArray(int[] arr)
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     System.Console.Write(arr[i] + " ");
+// }
+// System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+// }
 
-var myarray = GetArray();
-PrintArray(myarray);
+// var myarray = GetArray();
+// PrintArray(myarray);
---
+++ home_029/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, 
+// которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+int GetNumber(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GetArray()
+{
+    int[] arr = new int[8];
+    var rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(0, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+var myarray = GetArray();
+PrintArray(myarray);
