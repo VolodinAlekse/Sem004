ea10d9b674ef8d3c7d37e138e3d616b86e7e60f3
VolodinAlekse <volodin.8989@mail.ru>
2023-02-11 23:39:48 +0300

    demo_030_string.Join

--- demo_030/Program.cs
+++ demo_030/Program.cs
@@ -3,23 +3,55 @@
 // [1,0,1,1,0,1,0,0]
 
 
-void PrintArray(int[] array)
-{
-    int count = array.Length;
-    for (int i = 0; i < count; i++)
-    {
-        System.Console.Write($"{array[i]} ");
-    }
-}
-int[] FillArray( int size)
-{
-    int[] arrN = new int[size];
-    for (int i = 0; i < 8; i++)
-    {
-        arrN[i] = new Random().Next(0, 2);
-    }
-    return arrN;
-}
-int[] myarray = FillArray(8);
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         System.Console.Write($"{array[i]} ");
+//     }
+// }
+// int[] FillArray( int size)
+// {
+//     int[] arrN = new int[size];
+//     for (int i = 0; i < 8; i++)
+//     {
+//         arrN[i] = new Random().Next(0, 2);
+//     }
+//     return arrN;
+// }
+// int[] myarray = FillArray(8);
 
+// PrintArray(myarray);
+
+
+
+
+int GetNumber(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GetArray()
+{
+    int[] arr = new int[8];
+    var rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(0, 2);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    // for (int i = 0; i < arr.Length; i++)
+    // {
+    //     System.Console.Write(arr[i] + " ");
+    // }
+    System.Console.WriteLine("["+string.Join(", ", arr)+"]");
+}
+
+var myarray = GetArray();
 PrintArray(myarray);
